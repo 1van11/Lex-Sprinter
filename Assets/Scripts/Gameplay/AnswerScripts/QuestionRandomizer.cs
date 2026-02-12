d51f3dfc23d0ddb7be6ca4cd966e2595073f79db
kaishi_0720 <djhonnpaul20@gmail.com>
2026-02-13 03:04:17 +0800

    NewShelf
    rewards and functions

--- Assets/Scripts/Gameplay/AnswerScripts/QuestionRandomizer.cs
+++ Assets/Scripts/Gameplay/AnswerScripts/QuestionRandomizer.cs
@@ -628,6 +628,16 @@ void OnTriggerExit(Collider other)
             }
         }
     }
+    // Call this when player answers correctly
+    public void AddWordCount()
+    {
+        int currentWords = PlayerPrefs.GetInt("TotalWordCount", 0);
+        currentWords++;
+        PlayerPrefs.SetInt("TotalWordCount", currentWords);
+        PlayerPrefs.Save();
+
+        Debug.Log("Total Words: " + currentWords);
+    }
 
     public void HideClueText()
     {
--- Assets/Scripts/Gameplay/Player functions/PlayerFunctions.cs
+++ Assets/Scripts/Gameplay/Player functions/PlayerFunctions.cs
@@ -390,7 +390,14 @@ void OnTriggerEnter(Collider other)
                 wordsCollected++;
                 UpdateWordCountUI();
 
-                if (DailyTaskManager.Instance != null)
+                    int totalWords = PlayerPrefs.GetInt("TotalWordCount", 0);
+                    totalWords++;
+                    PlayerPrefs.SetInt("TotalWordCount", totalWords);
+                    PlayerPrefs.Save();
+
+                    Debug.Log("💾 TotalWordCount Saved: " + totalWords);
+
+                    if (DailyTaskManager.Instance != null)
                     DailyTaskManager.Instance
                         .CheckAndCompleteTask(correctWord);
             }
